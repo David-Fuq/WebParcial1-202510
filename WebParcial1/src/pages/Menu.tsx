2b8a594a8bddfd3ed6b2eaee429e08ab916626cb
David-Fuq <d.fuquen@uniandes.edu.co>
2025-03-16 17:05:24 -0500

    Fatla int'l

--- WebParcial1/src/pages/Menu.tsx
+++ WebParcial1/src/pages/Menu.tsx
@@ -1,7 +1,9 @@
 import styles from "./Parts.module.scss";
 import React from "react";
 import { Container, Navbar, Carousel, Row, Col, Card } from "react-bootstrap";
-import menu from "../assets/menu.svg"
+import menu from "../assets/menu.svg";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface ApiData{
     image: string;
@@ -9,8 +11,19 @@ interface ApiData{
 }
 
 function Menu () {
-    
-    
+
+    async function  getData() {
+        const  response = await axios.get<ApiData[]>(
+            "https://my.api.mockaroo.com/food.json?key=60a38cc0"
+        );
+        return response.data;
+    }
+
+    const {isSuccess, data } = useQuery({
+        queryKey: ["apiData"],
+        queryFn: getData,
+    })
+
 
     return (
         <div>
@@ -29,47 +42,32 @@ function Menu () {
 
       <Container fluid className="mt-4">
         <Carousel>
-          <Carousel.Item>
-            <img
-              className={`${styles.img} d-block w-100`}
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className={`${styles.img} d-block w-100`}
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className={`${styles.img} d-block w-100`}
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
-              alt="Third slide"
-            />
-          </Carousel.Item>
+        {isSuccess &&
+            data.slice(0, 3).map((item, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  src={item.image}
+                  className={`${styles.img} d-block w-100`}
+                  alt="Slide"
+                />
+              </Carousel.Item>
+            ))}
         </Carousel>
       </Container>
 
       <Container className="mt-5">
         <Row className="g-4">
-          {[
-            { title: "Cheese", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
-            { title: "Bread", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
-            { title: "Ham", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
-            { title: "Lettuce", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
-          ].map((item, idx) => (
-            <Col key={idx} md={3}>
-              <Card >
-                <Card.Img variant="top" src={item.img} />
-                <Card.Body className={`text-center ${styles.custom_card_title}`}>
-                  <Card.Title >{item.title}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+            {isSuccess && 
+            data.map((item, idx) => (
+                <Col key={idx} md={3}>
+                  <Card >
+                    <Card.Img variant="top" src={item.image} />
+                    <Card.Body className={`text-center ${styles.custom_card_title}`}>
+                      <Card.Title >{item.food}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
         </Row>
       </Container>
     </div>
