f7004264edb77589d116f6da67d6933b19380805
David-Fuq <d.fuquen@uniandes.edu.co>
2025-03-16 16:46:50 -0500

    Carrousel Ready

--- WebParcial1/src/pages/Home.tsx
+++ WebParcial1/src/pages/Home.tsx
@@ -1,34 +1,43 @@
 import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import styles from "./Home.module.scss"
 import { useNavigate } from "react-router";
+import menu from "../assets/menu.svg";
+import stores from "../assets/stores.svg";
+import cart from "../assets/cart.svg";
 
 
 
 function Home () {
     const navigate = useNavigate();
-    const handleLogin = () => {
-        navigate("/home");
+    const handleMenu = () => {
+        navigate("/menu");
+    };
+    const handleStores = () => {
+        navigate("/stores");
+    };
+    const handleCart = () => {
+        navigate("/cart");
     };
     return (
         <Container className={styles.login_container} fluid>
             <Row className={`d-flex justify-content-center`}>
-                <Col className="col-auto">
+                <Col className={`col-auto ${styles.rounded_col}`}>
                     <Card className={styles.login_card}>
-                        <Card.Body>
-                            <Card.Title className={``}> Log In</Card.Title>
-                            <Form onSubmit={handleLogin}>
-                            <Form.Group>
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" required/>
-                            </Form.Group>
-                                <Form.Group>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" required/>
-                                </Form.Group>
-                                <Button className="mt-2" type="submit">
-                                Login
-                                </Button>
-                            </Form>
+                    <Card.Body className={`d-flex justify-content-between align-items-center ${styles.card_body}`}>
+                    <Row className="w-100 d-flex justify-content-between text-center">
+                                <Col>
+                                    <img src={menu} alt="Menu" className={styles.icon} onClick={handleMenu}/>
+                                    <p className={styles.icon_text}>Menu</p>
+                                </Col>
+                                <Col>
+                                    <img src={stores} alt="Stores" className={styles.icon} onClick={handleStores} />
+                                    <p className={styles.icon_text}>Stores</p>
+                                </Col>
+                                <Col>
+                                    <img src={cart} alt="Cart" className={styles.icon} onClick={handleCart}/>
+                                    <p className={styles.icon_text}>Cart</p>
+                                </Col>
+                            </Row>
                         </Card.Body>
                     </Card>
                 </Col>
--- WebParcial1/src/pages/Menu.tsx
+++ WebParcial1/src/pages/Menu.tsx
@@ -1,6 +1,78 @@
+import styles from "./Parts.module.scss";
+import React from "react";
+import { Container, Navbar, Carousel, Row, Col, Card } from "react-bootstrap";
+import menu from "../assets/menu.svg"
+
+interface ApiData{
+    image: string;
+    food: string
+}
+
 function Menu () {
+    
+    
+
     return (
-        <h1>Hello from Menu</h1>
+        <div>
+      <Navbar style={{ backgroundColor: "rgb(16, 99, 114)" }} className="justify-content-center p-3">
+        <Navbar.Brand className="fs-3 text-uppercase text-white"
+        style={{
+            fontFamily: "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif",
+            fontSize: "2rem",
+            fontWeight: "bold",
+          }}>
+        <img src={menu} alt="Stores" className={styles.icon}/>
+            Menu
+        <img src={menu} alt="Stores" className={styles.icon}/>
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container fluid className="mt-4">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className={`${styles.img} d-block w-100`}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className={`${styles.img} d-block w-100`}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className={`${styles.img} d-block w-100`}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+
+      <Container className="mt-5">
+        <Row className="g-4">
+          {[
+            { title: "Cheese", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
+            { title: "Bread", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
+            { title: "Ham", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
+            { title: "Lettuce", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIZEH2iNVL709lCDoFPW91t2zxx-CAERDsIg&s" },
+          ].map((item, idx) => (
+            <Col key={idx} md={3}>
+              <Card >
+                <Card.Img variant="top" src={item.img} />
+                <Card.Body className={`text-center ${styles.custom_card_title}`}>
+                  <Card.Title >{item.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
     );
 
 };
